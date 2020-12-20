698e55d63ecb67ed57eda631c927e2fd8a61348f
d.amangeldy@aviata.me <OlZwfhz77>
2020-12-21 00:08:56 +0600

    added css for registration

--- movie/src/Authorization/Register.tsx
+++ movie/src/Authorization/Register.tsx
@@ -1,10 +1,12 @@
-import React, { ReactElement, useContext, useState } from 'react'
-import { Redirect } from 'react-router-dom';
+import React, { ReactElement, useContext, useState} from 'react'
+import {Redirect} from 'react-router-dom';
 import AuthContext from '../Context/AuthContext';
 import User from '../Model/User';
 import css from './Register.module.css'
 
-interface Props{
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+interface Props {
     lastIndex: number,
     register: (user: User) => void
 }
@@ -12,21 +14,33 @@ interface Props{
 export default function Register({lastIndex, register}: Props): ReactElement {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('')
+
     const authState = useContext(AuthContext)
 
     function createUser(): User {
-        let user: User = {id: lastIndex, username: username, password: password, favourites: []}
-        return user 
+        console.log("Username: " + username)
+        console.log("Password: " + password)
+        return {id: lastIndex, username: username, password: password, favourites: []}
+    }
+
+    function validate() {
+        if (username.length == 0 || password.length == 0 && email.length == 0) {
+            alert("There are some missing fields.")
+            return
+        }
+        register(createUser())
     }
 
     return (
-        <div>
-            <div className={css.input_container}>
-                Register
-                <input type='text' placeholder='username' onChange={(e)=>setUsername(e.target.value)}></input>
-                <input type='text' placeholder='password'onChange={(e)=>setPassword(e.target.value)}></input>
-                <button onClick={() => register(createUser())}>Register</button>
-                {authState.isLogged ? <Redirect to='/account' /> : null}
+        <div className={css.input_container}>
+            <div className={css.input_card}>
+                <h1>Registration</h1>
+                <input type='email' placeholder='Email...' onChange={(e) => setEmail(e.target.value)}/>
+                <input type='text' placeholder='Username...' onChange={(e) => setUsername(e.target.value)}/>
+                <input type='password' placeholder='Password...' onChange={(e) => setPassword(e.target.value)}/>
+                <button onClick={() => validate()}>Register</button>
+                {authState.isLogged ? <Redirect to='/account'/> : null}
             </div>
         </div>
     )
